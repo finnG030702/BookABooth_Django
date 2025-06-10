0e9d172ecf2f85c7927df9bcf3ee7d6aba2babdf
Finn Geddert <Finn.Geddert@t-systems.com>
2025-06-10 15:19:04 +0200

    Blocked bookings get deleted after 5 Minutes. Used APScheduler, probably has to be changed if the app goes into production

--- BookABooth_Django/apps.py
+++ BookABooth_Django/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class BookaboothDjangoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'BookABooth_Django'
+
+    def ready(self):
+        from . import scheduler
+        scheduler.start()
--- bookabooth/settings.py
+++ bookabooth/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_htmx',
-    'BookABooth_Django',
+    'BookABooth_Django.apps.BookaboothDjangoConfig',
 ]
 
 MIDDLEWARE = [
