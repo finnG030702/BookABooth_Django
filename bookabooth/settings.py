d7006796bc0a1059d14c5982adeeafc68ed15eb5
Finn Geddert <Finn.Geddert@t-systems.com>
2025-07-08 13:34:43 +0200

    Added HSTS

--- bookabooth/settings.py
+++ bookabooth/settings.py
@@ -143,3 +143,6 @@ LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+SECURE_HSTS_SECONDS = 31536000 # one year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
